cce4f4375d617f4a4b828b816c5ea997d75ae823
krishnagopaljha <theindianpsych@gmail.com>
2024-08-01 17:43:20 +0530

    Update encrypix.py

--- encrypix.py
+++ encrypix.py
@@ -53,4 +53,4 @@ def decrypt():
 if __name__ == "__main__":
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
